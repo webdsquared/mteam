b52160adefae6f2914ffd379deced006a11ad6c2
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-04-26 07:29:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mteam::Application.config.secret_token = 'a4f44d69bb8e0ea061d996f4ff4ed4e73ff5c6c961468d4a00f8b83298ef12128725663e1d3c55fd48b20986e35b064c60f61474ad0c09b6f4559b04dde4b0b2'
